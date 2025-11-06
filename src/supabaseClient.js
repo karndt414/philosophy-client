dd867f1b88da2245560add87d6ffb30dba4a6b2d
karndt414 <koree.arndt@gmail.com>
2025-11-06 11:11:06 -0600

    Vercel hosting

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yszddgrshowndfouborm.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzemRkZ3JzaG93bmRmb3Vib3JtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNTcxNDMsImV4cCI6MjA3NzkzMzE0M30.wHYapijM12SQmzq3a4HnFBKj0kY5iZ9bslaiD3aSZDg';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
